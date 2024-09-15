846ee59b142f3331107608e8947a9ec5884310fc
Quadratech188 <quadratech188@gmail.com>
2024-09-15 23:31:53 +0900

    feat: Add some wrapper functions to KeyStroke

--- lhk/core_wrapper.lua
+++ lhk/core_wrapper.lua
@@ -1,8 +1,26 @@
 local G = require 'lhk_core'
+local vkCode = require 'vkCode'
 
+---@enum lhk.KeyStroke.strokes
 G.KeyStroke.strokes = {
 	press = false,
 	release = true
 }
+
+---Create a new lhk.KeyStroke userdata
+---@param vkCode integer Virtual keycode of the keystroke
+---@param stroke lhk.KeyStroke.strokes Stroke of the keystroke (press / release)
+---@return lhk.KeyStroke
+function G.KeyStroke.fromVkCode(vkCode, stroke)
+	return G.KeyStroke.new(vkCode, nil, stroke)
+end
+
+---Create a new lhk.KeyStroke userdata
+---@param scanCode integer Virtual keycode of the keystroke
+---@param stroke lhk.KeyStroke.strokes Stroke of the keystroke (press / release)
+---@return lhk.KeyStroke
+function G.KeyStroke.fromScanCode(scanCode, stroke)
+	return G.KeyStroke.new(nil, scanCode, stroke)
+end
 
 return G
