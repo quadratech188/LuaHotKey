c31801d63ff96642d1724552213fd8d6fa360831
Quadratech188 <quadratech188@gmail.com>
2024-11-04 22:02:48 +0900

    feat: Simplify init.lua

--- lhk/init.lua
+++ lhk/init.lua
@@ -1,10 +1,7 @@
 local G = require 'lhk.core_wrapper'
-local modifiers = require 'lhk.modifiers'
-local KeyStrokes = require 'lhk.KeyStrokes'
-local utils = require 'lhk.utils'
 
-G.modifiers = modifiers
-G.KeyStrokes = KeyStrokes
-G.utils = utils
+G.modifiers = require 'lhk.modifiers'
+G.KeyStrokes = require 'lhk.KeyStrokes'
+G.utils = require 'lhk.utils'
 
 return G
